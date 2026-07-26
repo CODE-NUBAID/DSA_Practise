4f6d61e04c9c68b411832f5e6c6cc6576d3bb6a3
CODE-NUBAID <nubaid.uddin2007@gmail.com>
2026-07-27 00:36:35 +0530

    Implement twoSum method to find indices of target

--- two_sum.java
+++ two_sum.java
@@ -1,1 +1,32 @@
+import java.util.Arrays;
 
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        // Store value and original index together
+        int[][] numWithIndex = new int[nums.length][2];
+        for (int i = 0; i < nums.length; i++) {
+            numWithIndex[i][0] = nums[i];
+            numWithIndex[i][1] = i;
+        }
+
+        // Sort by value
+        Arrays.sort(numWithIndex, (a, b) -> Integer.compare(a[0], b[0]));
+
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left < right) {
+            int sum = numWithIndex[left][0] + numWithIndex[right][0];
+
+            if (sum == target) {
+                return new int[] { numWithIndex[left][1], numWithIndex[right][1] };
+            } else if (sum < target) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return new int[] {};
+    }
+}
