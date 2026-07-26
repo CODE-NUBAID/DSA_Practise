fedd107d19596cac4cd22679481d28a5e174c2e3
CODE-NUBAID <nubaid.uddin2007@gmail.com>
2026-07-27 00:38:41 +0530

    Fix duplicate HashMap declaration in two_sum.java
    Removed duplicate declaration of the 'numbers' HashMap.

--- two_sum.java
+++ two_sum.java
@@ -1,9 +1,7 @@
 import java.util.HashMap;
-
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-
-        HashMap<Integer, Integer> numbers = new HashMap<>();
+       HashMap<Integer, Integer> numbers = new HashMap<>();
 
         for (int i = 0; i < nums.length; i++) {
 
