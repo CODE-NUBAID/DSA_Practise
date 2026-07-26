b13d18faa7de9d0255f7fcdcb5a06b4b80add70a
CODE-NUBAID <nubaid.uddin2007@gmail.com>
2026-07-27 00:38:12 +0530

    Update two_sum.java

--- two_sum.java
+++ two_sum.java
@@ -1,32 +1,21 @@
-import java.util.Arrays;
+import java.util.HashMap;
 
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int[][] numWithIndex = new int[nums.length][2];
-        for (int i = 0; i < nums.length; i++) {
-            numWithIndex[i][0] = nums[i];
-            numWithIndex[i][1] = i;
-        }
 
-        Arrays.sort(numWithIndex, (a, b) -> Integer.compare(a[0], b[0]));
+        HashMap<Integer, Integer> numbers = new HashMap<>();
 
         for (int i = 0; i < nums.length; i++) {
-            int complement = target - numWithIndex[i][0];
-            
-            // Search for complement in the rest of the array
-            int low = i + 1, high = nums.length - 1;
-            while (low <= high) {
-                int mid = low + (high - low) / 2;
-                if (numWithIndex[mid][0] == complement) {
-                    return new int[] { numWithIndex[i][1], numWithIndex[mid][1] };
-                } else if (numWithIndex[mid][0] < complement) {
-                    low = mid + 1;
-                } else {
-                    high = mid - 1;
-                }
+
+            int need = target - nums[i];
+
+            if (numbers.containsKey(need)) {
+                return new int[]{numbers.get(need), i};
             }
+
+            numbers.put(nums[i], i);
         }
 
-        return new int[] {};
+        return new int[0];
     }
 }
