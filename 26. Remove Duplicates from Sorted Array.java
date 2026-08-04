4d469931442800cf23bd241a6586c5ce2f6739d5
CODE-NUBAID <nubaid.uddin2007@gmail.com>
2026-08-05 00:54:20 +0530

    Refactor removeDuplicates method for clarity
    Refactor code to improve readability by removing unnecessary comments.

--- 26. Remove Duplicates from Sorted Array.java
+++ 26. Remove Duplicates from Sorted Array.java
@@ -2,10 +2,10 @@ class Solution {
     public int removeDuplicates(int[] nums) {
         if (nums.length == 0) return 0;
 
-        int insertIndex = 1; // Pointer for the next unique position
+        int insertIndex = 1; 
 
         for (int i = 1; i < nums.length; i++) {
-            // Found a new unique element
+  
             if (nums[i] != nums[insertIndex - 1]) {
                 nums[insertIndex] = nums[i];
                 insertIndex++;
