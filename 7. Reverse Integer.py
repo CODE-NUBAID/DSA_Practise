7c421abb4b906feb35a74446df87023f47bced06
CODE-NUBAID <nubaid.uddin2007@gmail.com>
2026-08-05 00:47:21 +0530

    Add reverse integer method to Solution class
    Implement reverse integer function handling negative values and overflow.

--- 7. Reverse Integer.py
+++ 7. Reverse Integer.py
@@ -1,1 +1,19 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        negative = x < 0
+        x = abs(x)
 
+        reverse_num = 0
+
+        while x > 0:
+            digit = x % 10
+            reverse_num = reverse_num * 10 + digit
+            x //= 10
+
+        if negative:
+            reverse_num = -reverse_num
+
+        if reverse_num < -2**31 or reverse_num > 2**31 - 1:
+            return 0
+
+        return reverse_num
