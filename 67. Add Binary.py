6d761f651ecbe9f3714db2e2b383d2f6142d206b
CODE-NUBAID <nubaid.uddin2007@gmail.com>
2026-08-06 13:01:33 +0530

    Implement binary addition in addBinary method

--- 67. Add Binary.py
+++ 67. Add Binary.py
@@ -1,1 +1,7 @@
-
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        num_a = int(a, 2)
+        num_b = int(b, 2)
+        total = num_a + num_b
+        
+        return bin(total)[2:]
