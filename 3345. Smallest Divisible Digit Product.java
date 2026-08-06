f29cd45f2b593dc759017f384081c5046dc814c6
CODE-NUBAID <nubaid.uddin2007@gmail.com>
2026-08-07 00:13:22 +0530

    Add method to find smallest divisible digit product
    Implemented a method to find the smallest number whose digit product is divisible by a given integer.

--- 3345. Smallest Divisible Digit Product.java
+++ 3345. Smallest Divisible Digit Product.java
@@ -1,1 +1,20 @@
-
+class Solution {
+    public int smallestNumber(int n, int t) {
+        int x = n;
+        
+        while (true) {
+            String str = String.valueOf(x);
+            int product = 1;
+            
+            for (char ch : str.toCharArray()) {
+                product *= (ch - '0');
+            }
+            
+            if (product % t == 0) {
+                return x;
+            }
+            
+            x++;
+        }
+    }
+}
