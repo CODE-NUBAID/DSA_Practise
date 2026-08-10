224693265be8fec1d39767d9472ec9fdd9751ad0
CODE-NUBAID <nubaid.uddin2007@gmail.com>
2026-08-10 20:35:28 +0530

    Update 39. Combination Sum.java

--- 39. Combination Sum.java
+++ 39. Combination Sum.java
@@ -1,1 +1,24 @@
+class Solution {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> res = new ArrayList<>();
+        backtrack(0, target, new ArrayList<>(), res, candidates);
+        return res;
+    }
 
+    private void backtrack(int start, int remain, List<Integer> path, List<List<Integer>> res, int[] candidates) {
+        if (remain == 0) {
+            res.add(new ArrayList<>(path));
+            return;
+        }
+        if (remain < 0) {
+            return;
+        }
+
+        for (int i = start; i < candidates.length; i++) {
+            path.add(candidates[i]);
+        
+            backtrack(i, remain - candidates[i], path, res, candidates);
+            path.remove(path.size() - 1);
+        }
+    }
+}
