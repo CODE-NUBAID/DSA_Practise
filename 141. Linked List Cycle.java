64ce9b9c4cb9232de612041ac0994e35cde2c809
CODE-NUBAID <nubaid.uddin2007@gmail.com>
2026-08-12 20:42:16 +0530

    Implement cycle detection in linked list

--- 141. Linked List Cycle.java
+++ 141. Linked List Cycle.java
@@ -1,1 +1,19 @@
+import java.util.HashSet;
+import java.util.Set;
 
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        Set<ListNode> visitedNodes = new HashSet<>();
+        ListNode curr = head;
+
+        while (curr != null) {
+            if (visitedNodes.contains(curr)) {
+                return true;
+            }
+            visitedNodes.add(curr);
+            curr = curr.next;
+        }
+
+        return false;
+    }
+}
