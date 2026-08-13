c4591ec2b35d1e670cee1a3b5cc224cf284fc5d8
CODE-NUBAID <nubaid.uddin2007@gmail.com>
2026-08-13 12:16:14 +0530

    Implement function to identify duplicate emails
    Added a function to find duplicate emails in a DataFrame.

--- 182. Duplicate Emails.py
+++ 182. Duplicate Emails.py
@@ -1,1 +1,6 @@
+import pandas as pd
 
+def duplicate_emails(person: pd.DataFrame) -> pd.DataFrame:
+    grouped = person.groupby('email').size().reset_index(name='count')
+    duplicates = grouped[grouped['count'] > 1]
+    return duplicates[['email']].rename(columns={'email': 'Email'})
