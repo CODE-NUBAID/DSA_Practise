3f6c8de753c2654e38c0595579fb54439bbbdb01
CODE-NUBAID <nubaid.uddin2007@gmail.com>
2026-08-16 13:01:58 +0530

    Implement reverseString method to reverse array

--- 344. Reverse String.java
+++ 344. Reverse String.java
@@ -1,1 +1,16 @@
-
+class Solution {
+    public void reverseString(char[] s) {
+        int n = s.length;
+        char[] temp = new char[n];
+        
+        // Copy elements in reverse order to temp array
+        for (int i = 0; i < n; i++) {
+            temp[i] = s[n - 1 - i];
+        }
+        
+        // Overwrite original array with temp array
+        for (int i = 0; i < n; i++) {
+            s[i] = temp[i];
+        }
+    }
+}
