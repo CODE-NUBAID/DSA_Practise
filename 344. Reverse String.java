7e5ec53d7427834838c8f31d05f3d16dc23b9752
CODE-NUBAID <nubaid.uddin2007@gmail.com>
2026-08-16 13:02:14 +0530

    Clean up comments in reverseString method
    Removed comments for clarity.

--- 344. Reverse String.java
+++ 344. Reverse String.java
@@ -3,12 +3,10 @@ class Solution {
         int n = s.length;
         char[] temp = new char[n];
         
-        // Copy elements in reverse order to temp array
         for (int i = 0; i < n; i++) {
             temp[i] = s[n - 1 - i];
         }
         
-        // Overwrite original array with temp array
         for (int i = 0; i < n; i++) {
             s[i] = temp[i];
         }
