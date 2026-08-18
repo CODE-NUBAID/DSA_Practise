e1c37e3e20b4233f00f0a4da1bd0a65f78bdbcce
NUBAID UDDIN <nubaid.uddin2007@gmail.com>
2026-08-19 00:38:37 +0530

    Implement XOR operation in an array

--- 1486. XOR Operation in an Array.java
+++ 1486. XOR Operation in an Array.java
@@ -1,1 +1,12 @@
+class Solution {
+    public int xorOperation(int n, int start) {
+        int result = 0;
 
+        for (int i = 0; i < n; i++) {
+            int currentVal = start + 2 * i;
+            result ^= currentVal;
+        }
+
+        return result;
+    }
+}
