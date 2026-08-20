cddacfab658d33a087e526948de13b76f4232ca4
NUBAID UDDIN <nubaid.uddin2007@gmail.com>
2026-08-20 20:24:04 +0530

    Optimize minOperations method for efficiency
    Refactor the operation logic to calculate required increments in O(1) time and update the current element accordingly.

--- 1827. Minimum Operations to Make the Array Increasing.java
+++ 1827. Minimum Operations to Make the Array Increasing.java
@@ -1,12 +1,18 @@
 class Solution {
     public int minOperations(int[] nums) {
         int operations = 0;
+
         for (int i = 1; i < nums.length; i++) {
-            while (nums[i] <= nums[i - 1]) {
-                nums[i]++;
-                operations++;
+            if (nums[i] <= nums[i - 1]) {
+                // Calculate required operations in O(1) time
+                int expectedValue = nums[i - 1] + 1;
+                operations += expectedValue - nums[i];
+
+                // Update current element to minimum valid value
+                nums[i] = expectedValue;
             }
         }
+
         return operations;
     }
 }
